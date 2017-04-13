243564d4c5fb612131fa5b5697b70032fe2b17f4
reniatt <renatapyttel@gmail.com>
2017-04-13 23:11:33 +0200

    Create app.js

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+var main = function() {
+  $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
+
+    $(this).addClass('current');
+    $(this).children('.description').show();
+  });
+
+  $(document).keypress(function(event) {
+    if(event.which === 108) {
+      $('.description').hide();
+      $('.current').children('.description').show();
+    }
+
+    else if(event.which === 112) {
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.next();
+      
+      currentArticle.removeClass('current');
+      nextArticle.addClass('current');
+    }
+  });
+}
+
+$(document).ready(main);
