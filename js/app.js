851255c79fce939c29031ae066249018b1633379
reniatt <renatapyttel@gmail.com>
2017-04-18 19:03:41 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,16 +5,15 @@ var main = function() {
 
     $(this).addClass('current');
     $(this).children('.description').show();
-  });
+  });  
   
-  var LETTER_L_EVENT_CODE = 108;
-  var LETTER_P_EVENT_CODE = 112;
-    $(document).keypress(function(event) {
+  $(document).keypress(function(event) {
+    var LETTER_L_EVENT_CODE = 108;
     if(event.which === LETTER_L_EVENT_CODE) {
       $('.description').hide();
       $('.current').children('.description').show();
     }
-
+    var LETTER_P_EVENT_CODE = 112;
     else if(event.which === LETTER_P_EVENT_CODE) {
       var currentArticle = $('.current');
       var nextArticle = currentArticle.next();
