ae8e8d377e61d125e79e892cef913e5c82c164aa
reniatt <renatapyttel@gmail.com>
2017-04-20 17:22:28 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,10 +25,10 @@ var main = function() {
       
       else (event.which === LETTER_O_EVENT_CODE) {
         var curentArticle = $('.current');
-        var prevArticle = currentArticle.prev();
+        var prevArticle = currentArticle.previous();
         
         currentArticle.removeClass('current');
-        prevArticle.addClass('current');
+        previousArticle.addClass('current');
     }
   });
 }
