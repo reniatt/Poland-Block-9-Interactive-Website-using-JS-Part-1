1f6d6e1a5ffa0a928a349d3d40f59e1f8b637e57
reniatt <renatapyttel@gmail.com>
2017-07-11 18:35:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ var main = function() {
   });  
   
   $(document).keydown(function(event) {
-    
     if(event.which == 39) {
       $('.description').hide();
       $('.current').children('.description').show();
@@ -16,18 +15,12 @@ var main = function() {
     else if(event.which == 40) {
       var currentArticle = $('.current');
       var nextArticle = currentArticle.next();
-      
       currentArticle.removeClass('current');
       nextArticle.addClass('current');
-    }  
-  });
-
-  $(document).keydown(function(event) {
-	  
-    if(event.which == 38) {
+    }
+    else(event.which == 38) {
       var currentArticle = $('.current');
       var prevArticle = currentArticle.prev();
-      
       currentArticle.removeClass('current');
       prevArticle.addClass('current'); 
     }
