bc1ed0443274c4c90b5655692d7ae98d8981f77b
reniatt <renatapyttel@gmail.com>
2017-07-12 18:48:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,37 +8,28 @@ var main = function() {
   });  
   
   $(document).keydown(function(event) {
-    var RIGHT_ARROW_EVENT_CODE = 39
-    var DOWN_ARROW_EVENT_CODE = 40
+    var RIGHT_ARROW_KEY_CODE = 39;
+    var DOWN_ARROW_KEY_CODE = 40;
+	var UP_ARROW_KEY_CODE = 38;
 	
-    if(event.which == RIGHT_ARROW_EVENT_CODE) {
+    if(event.which == RIGHT_ARROW_KEY_CODE) {
       $('.description').hide();
       $('.current').children('.description').show();
     }
-    else if(event.which == DOWN_ARROW_EVENT_CODE) {
+    else if(event.which == DOWN_ARROW_KEY_CODE) {
       var currentArticle = $('.current');
       var nextArticle = currentArticle.next();
       
       currentArticle.removeClass('current');
       nextArticle.addClass('current');
-    }  
-  });
-
-  $(document).keydown(function(event) {
-    var RIGHT_ARROW_EVENT_CODE = 39
-    var UP_ARROW_EVENT_CODE = 38 
-    
-    if(event.which == RIGHT_ARROW_EVENT_CODE) {
-      $('.description').hide();
-      $('.current').children('.description').show();
-    }
-    else if(event.which == UP_ARROW_EVENT_CODE) {
+    } 
+      if(event.which == UP_ARROW_KEY_CODE) {
       var currentArticle = $('.current');
       var prevArticle = currentArticle.prev();
       
       currentArticle.removeClass('current');
       prevArticle.addClass('current'); 
-    }
+      }
   });
 }
 
