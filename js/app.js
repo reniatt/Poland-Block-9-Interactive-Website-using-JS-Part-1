64a570dce35264dbb97fc24079de25cfa155f054
reniatt <renatapyttel@gmail.com>
2017-07-14 10:39:11 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 var main = function() {
+    var RIGHT_ARROW_KEY_CODE = 39;
+    var DOWN_ARROW_KEY_CODE = 40;
+    var UP_ARROW_KEY_CODE = 38;
+	
   $('.article').click(function() {
     $('.article').removeClass('current');
     $('.description').hide();
@@ -8,43 +12,42 @@ var main = function() {
   });  
   
   $(document).keydown(function(event) {
-    var RIGHT_ARROW_KEY_CODE = 39;
-    var DOWN_ARROW_KEY_CODE = 40;
-    var UP_ARROW_KEY_CODE = 38;
 	
     if(event.which == RIGHT_ARROW_KEY_CODE) {
       $('.description').hide();
       $('.current').children('.description').show();
+	  
+      console.log('log test')
     }
     else if(event.which == DOWN_ARROW_KEY_CODE) {
       var currentArticle = $('.current');
-      var article = $('.articles');
+	  var article = $('.article');
       var nextArticle = currentArticle.next();
 	  
-      currentArticle.removeClass('current');
+	  currentArticle.removeClass('current');
 	  
-      if(nextArticle.lenght > 0) {
-      nextArticle.addClass('current');
+	  if(nextArticle.length > 0) {
+	  nextArticle.addClass('current');
       }
-      else if(nextArticle.lenght <= 0) { 
-      article.first().addClass('current');
+      else if(nextArticle.length <= 0) { 
+	  article.first().addClass('current');
       } 
     } 
     else if(event.which == UP_ARROW_KEY_CODE) {
        var currentArticle = $('.current');
-       var article = $('.article');
+	   var article = $('.article');
        var prevArticle = currentArticle.prev();
 	   
-       currentArticle.removeClass('current');
+	   currentArticle.removeClass('current');
 	   
-       if(prevArticle.lenght > 0) {
-       prevArticle.addClass('current');
+	   if(prevArticle.length > 0) {
+	   prevArticle.addClass('current');
        }
-       else if(prevArticle.lenght <= 0) {  
-       article.last().addClass('current');
+       else if(prevArticle.length <= 0) {  
+	   article.last().addClass('current');
        }  
-    }  
+	}  
   }); 
 }
 
-$(document).ready(main);
+$(main);
